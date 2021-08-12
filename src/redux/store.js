af99a9ff5f7ee507bfc58c8fa4a12ea500fada9a
fernandopaz1 <ferpaz9292@gmail.com>
2021-08-12 14:55:03 -0300

    add: archivos de configuracion de redux

---
+++ src/redux/store.js
@@ -0,0 +1,10 @@
+
+// constantes
+// aca se guardan la informacion de los pokemones
+
+// reducer 
+// el reducer segun las acciones que se ejecuten toma la informacion
+// y lo envia a las constante(estado)
+
+// acciones
+// las acciones son las funciones que acceden a la store
