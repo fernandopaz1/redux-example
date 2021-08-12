1691ce7421e333fbf54257e48766fa335c5fc7ae
fernandopaz1 <ferpaz9292@gmail.com>
2021-08-12 19:04:25 -0300

    feat: agregando la store al Provider

--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 // constantes
 // aca se guardan la informacion de los pokemones
 const dataInicial = {
-    array: [],
+    array: []
 }
 
 // defino los types de las actions con constantes
@@ -14,30 +14,30 @@ const OBTNENER_POKEMONES_EXITO = 'OBTNENER_POKEMONES_EXITO'
 // y lo envia a las constante(estado)
 // se recomienda que el estado inicial que toma el reducer sea 
 // dataInicial
-export default function pokeReducer(state= dataInicial, action){
+export default function pokesReducer(state = dataInicial, action){
+    // si es exitoso devuelvo los que estaban en el estado
+    // y hago append de los que me mandan en el payload
     switch(action.type){
-        // si es exitoso devuelvo los que estaban en el estado
-        // y hago append de los que me mandan en el payload
         case OBTNENER_POKEMONES_EXITO:
             return {...state, array: action.payload}
+        default:
+            return state
 
     }
 }
 
 // acciones
 // las acciones son las funciones que acceden a la store
-export const obtenerPokemonesAccion = () => {
-    // con dispatch vamos a activar el reducer y con getState accedemos a la data
-    async (dispatch, getState) => {
+// con dispatch vamos a activar el reducer y con getState accedemos a la data
+// si la llamada asincrona a la api da bien
+// llama al reducer con el tipo de accion exitoso
+// y le paso lo que obtuvimos de la api
+export const obtenerPokemonesAccion = () => async (dispatch, getState) => {
         try {
             const res = await axios.get('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')
-            // si la llamada asincrona a la api da bien
-            // llama al reducer con el tipo de accion exitoso
-            // y le paso lo que obtuvimos de la api
             dispatch({type: OBTNENER_POKEMONES_EXITO,
                     payload: res.data.results})
         } catch (error) {
             console.log(error)
         }
     }
-}
--- src/redux/store.js
+++ src/redux/store.js
@@ -4,20 +4,19 @@
 
 import {createStore, combineReducers, compose, applyMiddleware} from 'redux'
 import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
 
 // lo que importamos de los duck son los reducers que
-import pokesReducer from './pokesDucks'
+import pokesReducer from './pokeDucks'
 
 // aca conmbinamos todos los reducers 
-const rootReducer = combineReducers({
-    // en nuestros componenetes llamamos a los reducers con 
+ // en nuestros componenetes llamamos a los reducers con 
     // el nombre qde aca abajo
+const rootReducer = combineReducers({
     pokemones: pokesReducer
 })
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export default function generateStore() {
-    const store = createStore( rootReducer, composeEnhancers( applyMiddleware(thunk) ) )
-    return store
+    const store = createStore( rootReducer, composeEnhancers( applyMiddleware(thunk) ) );
+    return store;
 }
