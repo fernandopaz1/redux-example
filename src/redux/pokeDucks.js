54e22ff8112d7aad5023c40c318b45b5a4f5822b
fernandopaz1 <ferpaz9292@gmail.com>
2021-08-13 11:18:12 -0300

    feat: paginacion de pokemons con boton siguiente anterior

--- src/redux/pokeDucks.js
+++ src/redux/pokeDucks.js
@@ -6,11 +6,14 @@ const dataInicial = {
     array: [],
     offset: 0,
     limit: 20,
+    pagina: 0,
 }
 
 // defino los types de las actions con constantes
 const OBTNENER_POKEMONES_EXITO = 'OBTNENER_POKEMONES_EXITO'
 const SIGUIENTE_POKEMONES_EXITO = 'SIGUIENTE_POKEMONES_EXITO'
+const ANTERIOR_POKEMONES_EXITO = 'ANTERIOR_POKEMONES_EXITO'
+const ELIMINAR_POKEMONES_EXITO = 'ELIMINAR_POKEMONES_EXITO'
 
 // reducer 
 // el reducer segun las acciones que se ejecuten toma la informacion
@@ -24,9 +27,11 @@ export default function pokesReducer(state = dataInicial, action){
         case OBTNENER_POKEMONES_EXITO:
             return {...state, array: action.payload}
         case ELIMINAR_POKEMONES_EXITO:
-            return {array: []}
+            return {array: [], offset: 0, pagina: 0, limit: 20}
         case SIGUIENTE_POKEMONES_EXITO:
-            return {...state, array: action.payload, offset: action.payload.offset}
+            return {array: action.payload.array, offset: action.payload.offset, pagina: action.payload.pagina, limit: 20 }
+        case ANTERIOR_POKEMONES_EXITO:
+            return {array: action.payload.array, offset: action.payload.offset,pagina: action.payload.pagina, limit: 20}
         default:
             return state
 
@@ -42,9 +47,10 @@ export default function pokesReducer(state = dataInicial, action){
 export const obtenerPokemonesAccion = () => async (dispatch, getState) => {
     const offset = getState().pokemones.offset
     const limit = getState().pokemones.limit
+    
     const siguiente = offset + limit
     try {
-        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${siguiente}`)
         dispatch({type: OBTNENER_POKEMONES_EXITO,
                 payload: res.data.results})
     } catch (error) {
@@ -62,13 +68,41 @@ export const eliminarPokemonesAccion = () => async (dispatch, getState) => {
 }
 
 export const siguientePokemonAccion = () => async (dispatch, getState) => {
-    const offset = getState().pokemones.offset
+    // const offset = getState().pokemones.offset
     const limit = getState().pokemones.limit
+    const pagina = getState().pokemones.pagina
+    const offset = pagina*limit
     const siguiente = offset + limit
+    console.log(`Offset ${offset}`)
+    console.log(`siguiente ${siguiente}`)
+    console.log(`limit ${limit}`)
+    console.log(`pagina ${pagina}`)
+    console.log( `url : https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
     try {
         const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
         dispatch({type: SIGUIENTE_POKEMONES_EXITO,
-            payload: {array: res.data.results, offset: siguiente}})
+            payload: {array: res.data.results, offset: siguiente,  pagina: pagina+1}})
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+export const anteriorPokemonAccion = () => async (dispatch, getState) => {
+    // const offset = getState().pokemones.offset
+    const limit = getState().pokemones.limit
+    const pagina = getState().pokemones.pagina
+    const nextPage = pagina === 0 ? 0 : pagina - 1
+    const offset = limit*(pagina-1)
+    const siguiente = offset + limit
+    console.log(`Offset ${offset}`)
+    console.log(`siguiente ${siguiente}`)
+    console.log(`limit ${limit}`)
+    console.log(`pagina ${pagina}`)
+    console.log( `url : https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+    try {
+        const res = await axios.get(`https://pokeapi.co/api/v2/pokemon?offset=${offset}&limit=${limit}`)
+        dispatch({type: ANTERIOR_POKEMONES_EXITO,
+            payload: {array: res.data.results, offset: siguiente,  pagina: nextPage}})
     } catch (error) {
         console.error(error)
     }
